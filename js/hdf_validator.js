c07ef658fc42cda0dd8beeaddceda2b3d03a1207
mccedddy <cedricmacallan@gmail.com>
2024-01-04 01:01:04 +0800

    Added hidden question 6

--- js/hdf_validator.js
+++ js/hdf_validator.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Show / Hide Question 6
+  var hiddenQuestions = document.querySelector('.hidden-question');
+  hiddenQuestions.style.display = 'none';
+  var radioQ5 = document.getElementsByName('q5');
+  for (var i = 0; i < radioQ5.length; i++) {
+    radioQ5[i].addEventListener('change', function () {
+      if (this.value === 'yes-positive' || this.value === 'yes-negative' || this.value === 'yes-pending') {
+        hiddenQuestions.style.display = 'block';
+      } else if (this.value === 'no') {
+        hiddenQuestions.style.display = 'none';
+      }
+    });
+  }
+});
+
+// Popup Controller
 function openPerfectScoreModal() {
   var modal = document.getElementById("perfectScoreModal");
   modal.style.display = "block";
@@ -19,6 +36,6 @@ function closeConfirmationModal() {
 }
 
 function submitForm() {
-  closeConfirmationModal(); // Close the confirmation modal
-  openPerfectScoreModal(); // Show the congratulations modal
+  closeConfirmationModal();
+  openPerfectScoreModal();
 }
