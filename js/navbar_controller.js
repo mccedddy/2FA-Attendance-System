c9a1ebfe76b76b087e07aaf3d9ecf34cc416bc43
mccedddy <cedricmacallan@gmail.com>
2024-01-05 20:04:33 +0800

    fixed prof page responsiveness

--- js/navbar_controller.js
+++ js/navbar_controller.js
@@ -1,12 +1,9 @@
 function toggleMobileNavbar() {
     var mobileNavbar = document.querySelector('.navbar');
-    var hamburger = document.querySelector('.hamburger');
     if (mobileNavbar.style.display === 'flex') {
       mobileNavbar.style.display = 'none';
-      hamburger.style.display = 'block';
     } else {
       mobileNavbar.style.display = 'flex';
-      hamburger.style.display = 'none';
       mobileNavbar.style.justifyContent = 'center';
     }
   }
