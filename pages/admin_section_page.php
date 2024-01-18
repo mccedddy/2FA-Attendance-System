6c68ce11f2a79f1c1a00828fa1efb71a711fa74c
mccedddy <cedricmacallan@gmail.com>
2024-01-18 22:17:21 +0800

    fixed import button icon

--- pages/admin_section_page.php
+++ pages/admin_section_page.php
@@ -181,7 +181,7 @@ mysqli_free_result($classListResult);
           <label for="fileInput" class="custom-file-input" id="fileInputLabel">Choose File</label>
           <span class="file-name" id="fileName">No file chosen</span>
           <input type="file" id="fileInput" accept=".xlsx" />
-          <button class="import-export" id="import"><p>IMPORT DATA</p><img src="..\assets\images\icons\plus.svg"/></button>
+          <button class="import-export" id="import"><p>IMPORT DATA</p><img src="..\assets\images\icons\upload.svg"/></button>
           <button class="import-export" id="export"><p>EXPORT DATA</p><img src="..\assets\images\icons\download.svg"/></button>
         </div>
       </div>
