0bc767aea02aade6ee8fa50c133339bc4555ccff
mccedddy <cedricmacallan@gmail.com>
2024-01-29 13:35:22 +0800

    added verification status in HDF

--- services/verify_hdf.php
+++ services/verify_hdf.php
@@ -8,14 +8,19 @@ if (isset($_POST['UIDresult'])) {
   $time = date("H:i:s");
   $date = date('Y-m-d');
 
-  // Convert date and time to timestamp
-  $timestamp = strtotime($date . ' ' . $time);
-
   // SQL query to fetch student data
   $studentSQL = "SELECT last_name, student_number, section, nfc_uid FROM students WHERE nfc_uid = '$uid'";
   $studentStmt = mysqli_prepare($connection, $studentSQL);
   mysqli_stmt_execute($studentStmt);
   $result = mysqli_stmt_get_result($studentStmt);
+
+  // If no student found
+  if ($result == NULL || mysqli_num_rows($result) == 0) {
+    echo json_encode(['studentData' => ['last_name' => 'none', 'student_number' => 'none', 'nfc_uid' => $uid]]);
+    exit;
+  }
+
+  // Get student data
   $studentData = mysqli_fetch_assoc($result);
   $studentNumber = $studentData['student_number'];
   mysqli_free_result($result);
@@ -25,15 +30,20 @@ if (isset($_POST['UIDresult'])) {
 
   // SQL query to retrieve HDF data
   $hdfSQL = "SELECT * 
-          FROM hdf 
-          WHERE student_number = '$studentNumber' 
-          AND timestamp = '$timestamp'";
+             FROM hdf 
+             WHERE student_number = '$studentNumber'
+             AND DATE(timestamp) = '$date'";
   $hdfStmt = mysqli_prepare($connection, $hdfSQL);
   mysqli_stmt_execute($hdfStmt);
   $result = mysqli_stmt_get_result($hdfStmt);
 
   if ($result && mysqli_num_rows($result) > 0) {
     $hdfData = mysqli_fetch_assoc($result);
+    $hdfId = $hdfData["id"];
+
+    $hdfSQL = "UPDATE hdf SET verified = 'true' WHERE id = $hdfId";
+    $hdfStmt = mysqli_prepare($connection, $hdfSQL);
+    mysqli_stmt_execute($hdfStmt);
 
     // Response: HDF Data
     echo json_encode(['hdf' => $hdfData]);
