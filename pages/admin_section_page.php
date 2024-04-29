5b238a016a8aa09c249b15be60a43482d55c76c7
mccedddy <cedricmacallan@gmail.com>
2024-04-29 20:44:07 +0800

    overhauled section page

--- pages/admin_section_page.php
+++ pages/admin_section_page.php
@@ -116,40 +116,84 @@ mysqli_free_result($sectionsResult);
       href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="../css/admin_section_page.css" />
+    <link rel="stylesheet" href="../css/global.css" />
+    <link rel="stylesheet" href="../css/dashboard.css" />
+    <link rel="stylesheet" href="../css/section.css" />
+    <link rel="stylesheet" href="../css/modal.css" />
   </head>
   <body>
     <nav class="navbar">
-      <div class="navbar-top">
-        <img src="..\assets\images\icons\arrow_left.svg" id="closeNavbar" class="nav-button" onclick="toggleMobileNavbar()"/>
-        <a onclick="toAdminHomepage()"><img src="..\assets\images\logos\pup_logo.png" class="logo"/></a>
-        <a onclick="toSection()"><img src="..\assets\images\icons\group.svg" class="nav-button"/></a>
-        <a onclick="toSchedule()"><img src="..\assets\images\icons\table.svg" class="nav-button"/></a>
-        <a onclick="toSubjects()"><img src="..\assets\images\icons\book.svg" class="nav-button"/></a>
-        <a onclick="toAnalytics()"><img src="..\assets\images\icons\graph.svg" class="nav-button"/></a>
+      <div class="top">
+        <img
+          src="..\assets\images\icons\arrow_left.svg"
+          id="closeNavbar"
+          class="close-nav"
+          onclick="toggleMobileNavbar()"
+          alt="arrow left"
+        />
+        <a onclick="toAdminHomepage()"
+          ><img
+            src="..\assets\images\logos\pup_logo.png"
+            alt="pup logo"
+            class="logo"
+        /></a>
+        <a onclick="toSection()"
+          ><img
+            src="..\assets\images\icons\group.svg"
+            alt="group"
+            class="button"
+        /></a>
+        <a onclick="toSchedule()"
+          ><img
+            src="..\assets\images\icons\table.svg"
+            alt="table"
+            class="button"
+        /></a>
+        <a onclick="toSubjects()"
+          ><img src="..\assets\images\icons\book.svg" alt="book" class="button"
+        /></a>
+        <a onclick="toAnalytics()"
+          ><img
+            src="..\assets\images\icons\graph.svg"
+            alt="graph"
+            class="button"
+        /></a>
+        <a onclick="toSettings()"
+          ><img
+            src="..\assets\images\icons\settings.svg"
+            alt="settings"
+            class="button"
+        /></a>
       </div>
-      <form method="POST" class="logout-form">
-        <button type="submit" name="logout" class="logout-button">
-          <img src="..\assets\images\icons\logout.svg" class="nav-button"/>
+      <form method="POST" class="bottom">
+        <button type="submit" name="logout" class="logout">
+          <img
+            src="..\assets\images\icons\logout.svg"
+            alt="logout"
+            class="button"
+          />
         </button>
       </form>
     </nav>
     <section class="main">
       <div class="header">
         <div class="left">
-          <div class="mobile-navbar-toggle" onclick="toggleMobileNavbar()">
-            <img src="..\assets\images\icons\hamburger.svg" class="hamburger">
-          </div>
-          <a onclick="toAdminHomepage()"><h1>PUP HDF Attendance System</h1></a>
+          <img
+            src="..\assets\images\icons\hamburger.svg"
+            alt="hamburger"
+            class="hamburger"
+            onclick="toggleMobileNavbar()"
+          />
+          <h3 onclick="toAdminHomepage()" class="title">PUPHAS</h3>
         </div>
         <div class="right">
-          <h5>ADMIN</h5>
+          <h6>ADMIN</h6>
         </div>
       </div>
-      <h1 class="title">Computer Engineering Sections</h1>
+      <h2 class="page-title">Computer Engineering Sections</h2>
       <div class="add-section-container">
         <button class="add-section-button" onclick="openAddSectionModal()">
-          <img src="..\assets\images\icons\plus.svg" class="hamburger">
+          <img src="..\assets\images\icons\plus.svg">
           <p>ADD SECTION</p>
         </button>
       </div>
@@ -168,7 +212,7 @@ mysqli_free_result($sectionsResult);
       </div>
     </section>
 
-    <div id="addSectionModal" class="modal-blur">
+    <div id="addModal" class="modal-blur">
       <div class="modal-content">
         <div class="top-modal">
           <h6>ADD SECTION</h6>
@@ -181,32 +225,33 @@ mysqli_free_result($sectionsResult);
             <p>Section Number</p>
             <input type="text" name="section" class="year-section-textbox" required></input>
           </div>
-          <div class="add-button-container">
+          <div class="submit-button-container">
             <button type="submit" name="add-section" id="addButton" class="add-button">ADD</button>
           </div>
         </form>
       </div>
     </div>
 
-    <div id="deleteSectionModal" class="modal-blur">
+    <div id="deleteModal" class="modal-blur">
       <div class="modal-content">
         <div class="top-modal">
           <h6>DELETE SECTION</h6>
         </div>
-          <span class="close-modal" onclick="closeDeleteSectionModal()">&times;</span>
-          <img src="../assets/images/graphics/girl_trash.png" style="height: 40%; width: 40%;" />
-          <h5 id="deleteSectionMessage" style="margin-bottom: 10px;"></h5>
+        <span class="close-modal" onclick="closeDeleteSectionModal()">&times;</span>
+        <img src="../assets/images/graphics/girl_trash.png" style="height: 40%; width: 40%;" />
+        <form method="POST">
+          <h5 id="deleteSectionMessage" style="margin-bottom: 10px; text-align: center;"></h5>
           <p style="margin: 0px; text-align: center;">WARNING: All of the student data in this section will be deleted.</p>
           <p style="margin: 10px; text-align: center;">Are you sure you want to delete this section?</p>
-          <div class="add-button-container">
+          <div class="submit-button-container">
             <button type="submit" name="confirm-delete-section" id="deleteButton">DELETE</button>
           </div>
         </form>
       </div>
     </div>
 
-    <script src="../js/navbar_controller.js"></script>
     <script src="../js/section.js"></script>
+    <script src="../js/navbar_controller.js"></script>
     <script>
       function toLogin() {
         window.location.href = "../index.php";
@@ -237,21 +282,22 @@ mysqli_free_result($sectionsResult);
         return false;
       }
       function openAddSectionModal() {
-        var addSectionModal = document.getElementById("addSectionModal");
+        var addSectionModal = document.getElementById("addModal");
         addSectionModal.style.display = "block";
       }
       function closeAddSectionModal() {
-        var addSectionModal = document.getElementById("addSectionModal");
+        var addSectionModal = document.getElementById("addModal");
         addSectionModal.style.display = "none";
       }
       function openDeleteSectionModal() {
-        var addSectionModal = document.getElementById("deleteSectionModal");
+        var addSectionModal = document.getElementById("deleteModal");
         addSectionModal.style.display = "block";
       }
       function closeDeleteSectionModal() {
-        var addSectionModal = document.getElementById("deleteSectionModal");
+        var addSectionModal = document.getElementById("deleteModal");
         addSectionModal.style.display = "none";
       }
     </script>
   </body>
 </html>
+
