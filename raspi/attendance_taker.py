ffaf026356c19a1b7ec2ae8530162518ab716f8a
mccedddy <cedricmacallan@gmail.com>
2024-05-23 21:25:49 +0800

    removed attendance.db

--- raspi/attendance_taker.py
+++ raspi/attendance_taker.py
@@ -5,16 +5,13 @@ import os
 import pandas as pd
 import time
 import logging
-# import sqlite3
-# import datetime
 from datetime import datetime, timedelta
 import mysql.connector
 
-# db_host = '192.168.0.114'  # Replace with the IP address of your laptop
-db_host = 'localhost'  # Replace with the IP address of your laptop
-db_user = 'puphas'        # Replace with your MySQL username
-db_password = 'Puphas-2024'    # Replace with your MySQL password
-db_name = 'puphas'          # Replace with your database name
+db_host = 'localhost'
+db_user = 'puphas'
+db_password = 'Puphas-2024'
+db_name = 'puphas'
 
 # Dlib  / Use frontal face detector of Dlib
 detector = dlib.get_frontal_face_detector()
@@ -41,23 +38,6 @@ try:
         print("Connected to database:", record)
 except mysql.connector.Error as err:
     print(f"Error: {err}")
-
-# Create a connection to the database
-# conn = sqlite3.connect("attendance.db")
-# cursor = conn.cursor()
-
-
-
-# Create a table for the current date
-# current_date = datetime.datetime.now().strftime("%Y_%m_%d")  # Replace hyphens with underscores
-# table_name = "attendance"
-# create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (name TEXT, time TEXT, date DATE, UNIQUE(name, date))"
-# cursor.execute(create_table_sql)
-
-# Commit changes and close the connection
-# conn.commit()
-# conn.close()
-
 
 class Face_Recognizer:
     def __init__(self):
@@ -377,142 +357,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# def process(self, stream):
-#         # 1.  Get faces known from "features.all.csv"
-#         if self.get_face_database():
-#             while stream.isOpened():
-#                 self.frame_cnt += 1
-#                 logging.debug("Frame " + str(self.frame_cnt) + " starts")
-#                 flag, img_rd = stream.read()
-#                 kk = cv2.waitKey(1)
-
-#                 # 2.  Detect faces for frame X
-#                 faces = detector(img_rd, 0)
-
-#                 # 3.  Update cnt for faces in frames
-#                 self.last_frame_face_cnt = self.current_frame_face_cnt
-#                 self.current_frame_face_cnt = len(faces)
-
-#                 # 4.  Update the face name list in last frame
-#                 self.last_frame_face_name_list = self.current_frame_face_name_list[:]
-
-#                 # 5.  update frame centroid list
-#                 self.last_frame_face_centroid_list = self.current_frame_face_centroid_list
-#                 self.current_frame_face_centroid_list = []
-
-#                 # 6.1  if cnt not changes
-#                 if (self.current_frame_face_cnt == self.last_frame_face_cnt) and (
-#                         self.reclassify_interval_cnt != self.reclassify_interval):
-#                     logging.debug("scene 1:   No face cnt changes in this frame!!!")
-
-#                     self.current_frame_face_position_list = []
-
-#                     if "unknown" in self.current_frame_face_name_list:
-#                         self.reclassify_interval_cnt += 1
-
-#                     if self.current_frame_face_cnt != 0:
-#                         for k, d in enumerate(faces):
-#                             self.current_frame_face_position_list.append(tuple(
-#                                 [faces[k].left(), int(faces[k].bottom() + (faces[k].bottom() - faces[k].top()) / 4)]))
-#                             self.current_frame_face_centroid_list.append(
-#                                 [int(faces[k].left() + faces[k].right()) / 2,
-#                                  int(faces[k].top() + faces[k].bottom()) / 2])
-
-#                             img_rd = cv2.rectangle(img_rd,
-#                                                    tuple([d.left(), d.top()]),
-#                                                    tuple([d.right(), d.bottom()]),
-#                                                    (255, 255, 255), 2)
-
-#                     #  Multi-faces in current frame, use centroid-tracker to track
-#                     if self.current_frame_face_cnt != 1:
-#                         self.centroid_tracker()
-
-#                     for i in range(self.current_frame_face_cnt):
-#                         # 6.2 Write names under ROI
-#                         img_rd = cv2.putText(img_rd, self.current_frame_face_name_list[i],
-#                                              self.current_frame_face_position_list[i], self.font, 0.8, (0, 255, 255), 1,
-#                                              cv2.LINE_AA)
-#                     self.draw_note(img_rd)
-
-#                 # 6.2  If cnt of faces changes, 0->1 or 1->0 or ...
-#                 else:
-#                     logging.debug("scene 2: / Faces cnt changes in this frame")
-#                     self.current_frame_face_position_list = []
-#                     self.current_frame_face_X_e_distance_list = []
-#                     self.current_frame_face_feature_list = []
-#                     self.reclassify_interval_cnt = 0
-
-#                     # 6.2.1  Face cnt decreases: 1->0, 2->1, ...
-#                     if self.current_frame_face_cnt == 0:
-#                         logging.debug("  / No faces in this frame!!!")
-#                         # clear list of names and features
-#                         self.current_frame_face_name_list = []
-#                     # 6.2.2 / Face cnt increase: 0->1, 0->2, ..., 1->2, ...
-#                     else:
-#                         logging.debug("  scene 2.2  Get faces in this frame and do face recognition")
-#                         self.current_frame_face_name_list = []
-#                         for i in range(len(faces)):
-#                             shape = predictor(img_rd, faces[i])
-#                             self.current_frame_face_feature_list.append(
-#                                 face_reco_model.compute_face_descriptor(img_rd, shape))
-#                             self.current_frame_face_name_list.append("unknown")
-
-#                         # 6.2.2.1 Traversal all the faces in the database
-#                         for k in range(len(faces)):
-#                             logging.debug("  For face %d in current frame:", k + 1)
-#                             self.current_frame_face_centroid_list.append(
-#                                 [int(faces[k].left() + faces[k].right()) / 2,
-#                                  int(faces[k].top() + faces[k].bottom()) / 2])
-
-#                             self.current_frame_face_X_e_distance_list = []
-
-#                             # 6.2.2.2  Positions of faces captured
-#                             self.current_frame_face_position_list.append(tuple(
-#                                 [faces[k].left(), int(faces[k].bottom() + (faces[k].bottom() - faces[k].top()) / 4)]))
-
-#                             # 6.2.2.3
-#                             # For every faces detected, compare the faces in the database
-#                             for i in range(len(self.face_features_known_list)):
-#                                 #
-#                                 if str(self.face_features_known_list[i][0]) != '0.0':
-#                                     e_distance_tmp = self.return_euclidean_distance(
-#                                         self.current_frame_face_feature_list[k],
-#                                         self.face_features_known_list[i])
-#                                     logging.debug("      with person %d, the e-distance: %f", i + 1, e_distance_tmp)
-#                                     self.current_frame_face_X_e_distance_list.append(e_distance_tmp)
-#                                 else:
-#                                     #  person_X
-#                                     self.current_frame_face_X_e_distance_list.append(999999999)
-
-#                             # 6.2.2.4 / Find the one with minimum e distance
-#                             similar_person_num = self.current_frame_face_X_e_distance_list.index(
-#                                 min(self.current_frame_face_X_e_distance_list))
-
-#                             if min(self.current_frame_face_X_e_distance_list) < 0.4:
-#                                 self.current_frame_face_name_list[k] = self.face_name_known_list[similar_person_num]
-#                                 logging.debug("  Face recognition result: %s",
-#                                               self.face_name_known_list[similar_person_num])
-
-#                                 # Insert attendance record
-#                                 nam = self.face_name_known_list[similar_person_num]
-
-#                                 print(type(self.face_name_known_list[similar_person_num]))
-#                                 print(nam)
-#                                 self.attendance(nam)
-#                             else:
-#                                 logging.debug("  Face recognition result: Unknown person")
-
-#                         # 7.  / Add note on cv2 window
-#                         self.draw_note(img_rd)
-
-#                 # 8.  'q'  / Press 'q' to exit
-#                 if kk == ord('q'):
-#                     break
-
-#                 self.update_fps()
-#                 cv2.namedWindow("camera", 1)
-#                 cv2.imshow("camera", img_rd)
-
-#                 logging.debug("Frame ends\n\n")
--- test/notes.md
+++ test/notes.md
@@ -1,24 +1,3 @@
 ADMIN CLASSLIST
 
-// 1. upload features should:
-
-- insert data from features_all.csv to db. (done)
-- if student has existing features on db, update. else, insert to features table. (done)
-
-// 2. clear data should: (done)
-
-- delete student features on the db (features table).
-- delete student folder on captures (../captures/2020-XXXXX-MN-0).
-- delete student features on features_all.csv (../captures/features_all.csv)
-
-// 3. registered column on classlist should be reflected on whether student has features on features table. (done)
-
-4. add alerts:
-
-- images processed (done)
-- featuress uploaded (done)
-- cleared features
-
-// 5. what if: process images + upload features in 1 button. After processing images, automatically upload features to db. (done)
-
-6. what if: after uploading, delete features_all.csv and student folders in ../captures.
+1. Fix section format
